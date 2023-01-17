b3a0986d3be8cd65352392db2be89c687d462f31
chae minkyoung <chea220215@gmail.com>
2023-01-17 17:10:33 +0900

    7-11.1

--- day04.py
+++ day04.py
@@ -1,3 +1,19 @@
-range=[number for number in range(10) if number%2==0]
-print(range)
+start1 = ["fee", "fie", "foe"]
+rhymes = [
+ ("flop", "get a mop"),
+ ("fope", "turn the rope"),
+ ("fa", "get your ma"),
+ ("fudge", "call the judge"),
+ ("fat", "pet the cat"),
+ ("fog", "walk the dog"),
+ ("fun", "say we're done"),
+ ]
+start2 = "Someone better"
+start1[0]=start1[0].capitalize()
+start1='! '.join(start1)
+print(start1)
+for i in range(0, 6):
+    rhymes1=''.join(rhymes[i][0]).capitalize()
+    print('! '+ rhymes1)
+
 
