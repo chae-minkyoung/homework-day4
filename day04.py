1f244c50e851fc7cbd8c25a328bc8d4ec81fee01
chae minkyoung <chea220215@gmail.com>
2023-01-17 16:19:37 +0900

    7-10

--- day04.py
+++ day04.py
@@ -1,9 +1,3 @@
-surprise=["Groucho","Chico","Harpo"]
-surprise[2]=surprise[2].lower()
-print(surprise)
-surprise=surprise[: : -1]
-surprise[0]=surprise[0].capitalize()
-print(surprise)
-
-
+range=[number for number in range(10) if number%2==0]
+print(range)
 
